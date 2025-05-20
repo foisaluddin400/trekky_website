a4b7fa4a765ee669a44edbe47a27c4308f36129e
foisaluddin400 <foisalrk2@gmail.com>
2025-05-20 12:24:35 +0600

    wer

--- src/Pages/information/Information.jsx
+++ src/Pages/information/Information.jsx
@@ -11,7 +11,7 @@ const Information = () => {
     const handleSubmit = (values) => {
         console.log(values)
     };
-    //dasdfkljasdfkjhasdfkjhasdfhksjdf
+    //dasdfkljasdfkjhasdfkjhasdfhksjd
     return (
         <div className='container m-auto'>
 
