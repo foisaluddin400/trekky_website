52638905bc4e1298e4bc47402f262c51d23a253b
foisaluddin400 <foisalrk2@gmail.com>
2025-06-03 14:55:13 +0600

    wer

--- src/Pages/rvSold/RvSoldInformationForm.jsx
+++ src/Pages/rvSold/RvSoldInformationForm.jsx
@@ -25,7 +25,7 @@ const props = {
         console.log('Dropped files', e.dataTransfer.files);
     },
 };
-//s
+//
 const RvSoldInformationForm = () => {
     const [form] = Form.useForm();
     const handleSubmit = (values) => {
